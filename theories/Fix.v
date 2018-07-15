aad1f202802e8969afbc6516b3f11918c97a33d8
Gregory Malecha <gmalecha@gmail.com>
2018-07-14 22:56:05 -0400

    a simpler definition that doesn't require Monad.

--- theories/Fix.v
+++ theories/Fix.v
@@ -18,15 +18,15 @@ Module Type FixSig.
     Variable codom : dom -> Type.
 
     Definition fix_body : Type :=
-      forall m, Monad m ->
-           (forall t, itree E t -> m t) ->
-           (forall x : dom, m (codom x)) ->
-           forall x : dom, m (codom x).
+      forall E',
+        (forall t, itree E t -> itree E' t) ->
+        (forall x : dom, itree E' (codom x)) ->
+        forall x : dom, itree E' (codom x).
 
     Parameter mfix : fix_body -> forall x : dom, itree E (codom x).
 
     Axiom mfix_unfold : forall (body : fix_body) (x : dom),
-        mfix body x = body (itree E) _ (fun t => id) (mfix body) x.
+        mfix body x = body E (fun t => id) (mfix body) x.
 
   End Fix.
 End FixSig.
@@ -92,21 +92,22 @@ Module FixImpl : FixSig.
        * complex, though one could argue that it is a more abstract encoding.
        *)
       Definition fix_body : Type :=
-        forall m, Monad m ->
-             (forall t, itree E t -> m t) ->
-             (forall x : dom, m (codom x)) ->
-             forall x : dom, m (codom x).
+        forall E',
+          (forall t, itree E t -> itree E' t) ->
+          (forall x : dom, itree E' (codom x)) ->
+          forall x : dom, itree E' (codom x).
 
       Variable body : fix_body.
 
       Definition mfix
       : forall x : dom, itree E (codom x) :=
         _mfix
-          (body (itree (E +' fixpoint)) _ (fun t : Type => hoist (fun X : Type => inl))
+          (body (E +' fixpoint)
+                (fun t : Type => hoist (fun X : Type => inl))
                 (fun x0 : dom => Vis (inr (call x0)) Ret)).
 
       Theorem mfix_unfold : forall x,
-          mfix x = body (itree E) _ (fun t => id) mfix x.
+          mfix x = body E (fun t => id) mfix x.
       Proof. Admitted.
     End mfixP.
   End Fix.
