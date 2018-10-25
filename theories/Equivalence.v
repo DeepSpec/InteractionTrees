64c417efe6be61e7437e374d375f9a6ac9c1931a
Lysxia <lysxia@gmail.com>
2018-10-25 18:09:08 -0400

    Rename untaus/untaus' to unalltaus/untaus

--- theories/Equivalence.v
+++ theories/Equivalence.v
@@ -23,9 +23,9 @@ Inductive eutt_0 (eutt : relation (itree E R)) :
     eutt_0 eutt (Vis e k1) (Vis e k2).
 Hint Constructors eutt_0.
 
-Inductive untaus' t' : itree E R -> Prop :=
-| OneTau : forall t, untaus' t' t -> untaus' t' (Tau t)
-| NoTau : untaus' t' t'
+Inductive untaus t' : itree E R -> Prop :=
+| OneTau : forall t, untaus t' t -> untaus t' (Tau t)
+| NoTau : untaus t' t'
 .
 
 Definition notau (t : itree E R) :=
@@ -34,16 +34,16 @@ Definition notau (t : itree E R) :=
   | _ => True
   end.
 
-Definition untaus t t' := notau t' /\ untaus' t' t.
+Definition unalltaus t t' := notau t' /\ untaus t' t.
 
-Definition finite_taus t := exists t', untaus t t'.
+Definition finite_taus t := exists t', unalltaus t t'.
 
 Definition is_spin (t : itree E R) := t = spin.
 
 Definition eutt_ eutt : relation _ := fun t1 t2 =>
   (finite_taus t1 <-> finite_taus t2) /\
   forall t1' t2',
-    untaus t1 t1' -> untaus t2 t2' ->
+    unalltaus t1 t1' -> unalltaus t2 t2' ->
     eutt_0 eutt t1' t2'.
 
 Lemma monotone_eutt_0 : monotone2 eutt_0.
@@ -72,15 +72,15 @@ Infix "~" := eutt
 
 (**)
 
-Lemma notau_untaus (t : itree E R) : notau t -> untaus t t.
+Lemma notau_unalltaus (t : itree E R) : notau t -> unalltaus t t.
 Proof. split; [ auto | constructor ]. Qed.
 
-Lemma untaus_untaus' (t t' : itree E R) :
-  untaus t t' -> untaus' t' t.
+Lemma unalltaus_untaus (t t' : itree E R) :
+  unalltaus t t' -> untaus t' t.
 Proof. firstorder. Qed.
 
-Lemma untaus_injective : forall t t1 t2,
-    untaus t t1 -> untaus t t2 -> t2 = t1.
+Lemma unalltaus_injective : forall t t1 t2,
+    unalltaus t t1 -> unalltaus t t2 -> t2 = t1.
 Proof.
   intros t t1 t2 [I1 H1] [I2 H2].
   induction H1 as [t1_ H1 IH1| ];
@@ -88,13 +88,13 @@ Proof.
       subst; contradiction.
 Qed.
 
-Lemma untaus_notau : forall t t', ~untaus t (Tau t').
+Lemma unalltaus_notau : forall t t', ~unalltaus t (Tau t').
 Proof.
   intros t t' [I H]; auto.
 Qed.
 
-Lemma untaus_tau (t1 t2 : itree E R) :
-  untaus (Tau t1) t2 <-> untaus t1 t2.
+Lemma unalltaus_tau (t1 t2 : itree E R) :
+  unalltaus (Tau t1) t2 <-> unalltaus t1 t2.
 Proof.
   split; intros [I1 H1].
   - inversion H1; subst.
@@ -134,18 +134,18 @@ Proof.
   - exists t'; split; [ | apply OneTau ]; assumption.
 Qed.
 
-Lemma untaus'_finite_taus (t t' : itree E R) :
-    untaus' t t' -> (finite_taus t <-> finite_taus t').
+Lemma untaus_finite_taus (t t' : itree E R) :
+    untaus t t' -> (finite_taus t <-> finite_taus t').
 Proof.
   induction 1.
   - rewrite finite_taus_Tau; assumption.
   - reflexivity.
 Qed.
 
-Lemma untaus_finite_taus (t t' : itree E R) :
-    untaus t t' -> (finite_taus t <-> finite_taus t').
+Lemma unalltaus_finite_taus (t t' : itree E R) :
+    unalltaus t t' -> (finite_taus t <-> finite_taus t').
 Proof.
-  intros []. symmetry. apply untaus'_finite_taus; assumption.
+  intros []. symmetry. apply untaus_finite_taus; assumption.
 Qed.
 
 (**)
@@ -154,10 +154,10 @@ Lemma eutt_finite_taus (t1 t2 : itree E R) :
   t1 ~ t2 -> finite_taus t1 <-> finite_taus t2.
 Proof. intro H; punfold H; apply H. Qed.
 
-Lemma eutt_untaus_1 (t1 t2 : itree E R) :
+Lemma eutt_unalltaus_1 (t1 t2 : itree E R) :
   t1 ~ t2 -> finite_taus t1 ->
   exists t1' t2',
-    untaus t1 t1' /\ untaus t2 t2' /\ eutt_0 eutt t1' t2'.
+    unalltaus t1 t1' /\ unalltaus t2 t2' /\ eutt_0 eutt t1' t2'.
 Proof.
   intros Heutt Ht1. punfold Heutt.
   destruct Heutt as [Hfinite Heutt0].
@@ -193,7 +193,7 @@ Proof.
   split.
   - reflexivity.
   - intros t1 t2 H1 H2.
-    erewrite (untaus_injective _ _ _ H1 H2).
+    erewrite (unalltaus_injective _ _ _ H1 H2).
     apply reflexive_eutt_0; auto.
     firstorder.
 Qed.
@@ -266,23 +266,23 @@ Proof.
   split.
   - apply finite_taus_Tau.
   - intros t1' t2' H1 H2.
-    rewrite untaus_tau in H1.
-    pose proof (untaus_injective _ _ _ H1 H2); subst.
+    rewrite unalltaus_tau in H1.
+    pose proof (unalltaus_injective _ _ _ H1 H2); subst.
     apply reflexive_eutt_0.
     { left; apply Reflexive_eutt. }
     { apply H1. }
 Qed.
 
-Lemma untaus_equiv (t t' : itree E R) : untaus t t' -> t ~ t'.
+Lemma unalltaus_equiv (t t' : itree E R) : unalltaus t t' -> t ~ t'.
 Proof.
   intros H.
   pfold. split.
-  - apply untaus_finite_taus; assumption.
+  - apply unalltaus_finite_taus; assumption.
   - destruct H. induction H0.
-    + intros t1' t2' H1 H2. apply IHuntaus'.
-      apply untaus_tau; assumption. assumption.
+    + intros t1' t2' H1 H2. apply IHuntaus.
+      apply unalltaus_tau; assumption. assumption.
     + intros t1' t2' H1 H2.
-      rewrite (untaus_injective _ _ _ H1 H2).
+      rewrite (unalltaus_injective _ _ _ H1 H2).
       apply reflexive_eutt_0.
       { left; apply Reflexive_eutt. }
       { apply H1. }
@@ -301,20 +301,20 @@ Lemma finite_taus_bind_fst {E R S}
       (t : itree E R) (f : R -> itree E S) :
   finite_taus (t >>= f) -> finite_taus t.
 Proof.
-  intros [tf' [Hnotau Huntaus]].
+  intros [tf' [Hnotau Hunalltaus]].
   remember (t >>= f)%itree as tf eqn:Etf.
   generalize dependent t.
-  induction Huntaus; intros t' Etf;
+  induction Hunalltaus; intros t' Etf;
     rewrite match_bind in Etf; destruct t'; inversion Etf;
     try (apply finite_taus_notau; constructor).
   - apply finite_taus_Tau.
-    apply IHHuntaus; auto.
+    apply IHHunalltaus; auto.
   - subst tf'; inversion Hnotau.
 Qed.
 
-Lemma finite_taus_bind_untaus {E R S}
+Lemma finite_taus_bind_unalltaus {E R S}
       (t t' : itree E R) (k : R -> itree E S) :
-  untaus' t' t ->
+  untaus t' t ->
   finite_taus (t >>= k) <-> finite_taus (t' >>= k).
 Proof.
   induction 1.
@@ -329,13 +329,13 @@ Lemma finite_taus_bind {E R S}
     finite_taus (t1 >>= k1) -> finite_taus (t2 >>= k2).
 Proof.
   intros Ht Hk Hfin.
-  destruct (eutt_untaus_1 t1 t2) as [t1' [t2' [Ht1' [Ht2' Ht12']]]].
+  destruct (eutt_unalltaus_1 t1 t2) as [t1' [t2' [Ht1' [Ht2' Ht12']]]].
   { auto. }
   { eapply finite_taus_bind_fst; eauto. }
-  rewrite finite_taus_bind_untaus
-    by eauto using untaus_untaus'.
-  rewrite finite_taus_bind_untaus in Hfin
-    by eauto using untaus_untaus'.
+  rewrite finite_taus_bind_unalltaus
+    by eauto using unalltaus_untaus.
+  rewrite finite_taus_bind_unalltaus in Hfin
+    by eauto using unalltaus_untaus.
   rewrite match_bind in Hfin; rewrite match_bind.
   inversion Ht12'; subst; simpl in *.
   - rewrite <- eutt_finite_taus; eauto.
@@ -349,27 +349,27 @@ Proof.
   destruct t; constructor + contradiction.
 Qed.
 
-Lemma untaus_bind {E R S} (t : itree E R) (k : R -> itree E S) tk' :
-  untaus (t >>= k) tk' ->
-  (exists t', untaus t t' /\ untaus (t' >>= k) tk').
+Lemma unalltaus_bind {E R S} (t : itree E R) (k : R -> itree E S) tk' :
+  unalltaus (t >>= k) tk' ->
+  (exists t', unalltaus t t' /\ unalltaus (t' >>= k) tk').
 Proof.
-  intros [Hnotau Huntaus].
+  intros [Hnotau Hunalltaus].
   remember (t >>= k)%itree as tk eqn:Etk.
   generalize dependent t.
-  induction Huntaus; intros t' Etk'; subst.
+  induction Hunalltaus; intros t' Etk'; subst.
   - rewrite match_bind in Etk'.
     destruct t'; simpl in Etk'; inversion Etk'; subst.
     + exists (Ret r). split.
       * do 2 constructor.
       * rewrite match_bind; simpl; rewrite <- Etk'.
-        rewrite untaus_tau.
+        rewrite unalltaus_tau.
         split; auto.
-    + edestruct IHHuntaus as [t2 [Ht2 Htk2]]; eauto.
+    + edestruct IHHunalltaus as [t2 [Ht2 Htk2]]; eauto.
       eexists; split; eauto.
-      rewrite untaus_tau; auto.
+      rewrite unalltaus_tau; auto.
   - exists t'. split.
-    + eapply notau_untaus, notau_bind; eauto.
-    + apply notau_untaus; auto.
+    + eapply notau_unalltaus, notau_bind; eauto.
+    + apply notau_unalltaus; auto.
 Qed.
 
 Lemma eutt_bind {E R S} (t1 t2 : itree E R) (k1 k2 : R -> itree E S) :
@@ -383,8 +383,8 @@ Proof.
   - split; apply finite_taus_bind; auto;
       intros; symmetry; auto.
   - intros tk1' tk2' H1' H2'.
-    pose proof (untaus_bind _ _ _ H1') as [t1' [Ht1 Htk1]].
-    pose proof (untaus_bind _ _ _ H2') as [t2' [Ht2 Htk2]].
+    pose proof (unalltaus_bind _ _ _ H1') as [t1' [Ht1 Htk1]].
+    pose proof (unalltaus_bind _ _ _ H2') as [t2' [Ht2 Htk2]].
     punfold Ht.
     destruct Ht as [_ Ht].
     destruct (Ht _ _ Ht1 Ht2).
