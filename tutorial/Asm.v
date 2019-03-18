d6c2d006dd4ef8760b80a6d9c4bd7bf0f3562ac6
Yannick <y.zakowski@gmail.com>
2019-03-18 00:37:50 -0400

    Improvement in Asm. Fixed the missing evaluator. Note: I think the Exit event cannot be handled, though it's not much of an issue

--- tutorial/Asm.v
+++ tutorial/Asm.v
@@ -1,3 +1,5 @@
+(** * The Asm language  *)
+
 (** We now consider as a target a simple control-flow-graph language,
     so-called _Asm_.
     Computations are represented as a collection of basic blocks
@@ -50,7 +52,7 @@ Variant instr : Set :=
 
 (** We consider both direct and conditional jumps *)
 Variant branch {label : Type} : Type :=
-| Bjmp (_ : label) (* jump to label *)
+| Bjmp (_ : label)                (* jump to label *)
 | Bbrz (_ : var) (yes no : label) (* conditional jump *)
 | Bhalt
 .
@@ -96,7 +98,7 @@ Arguments code {A B}.
 (** _Asm_ produces two kind of effects: through manipulation of the store
     and of the heap.
     We therefore reuse _Imp_'s [Locals], and define an additional pair of
-    effects [Memory].
+    effects [Memory] to model interactions with the heap.
 *)
 Import Imp.
 
@@ -117,7 +119,7 @@ Section Denote.
   (** We introduce a special effect to model termination of the computation.
       Note that it expects _actively_ no answer from the environment: 
       [Done] is of type [Exit void].
-      We can therefore use it to close a [itree E A] no matter what the expected
+      We can therefore use it to "close" an [itree E A] no matter what the expected
       return type [A] is, as witnessed by the [done] computation.
    *)
   Inductive Exit : Type -> Type :=
@@ -203,7 +205,7 @@ Section Denote.
       (** A labelled collection of blocks, [bks], is simply the pointwise
           application of [denote_block].
           However, its denotation is therefore crucially a [ktree],
-          whose structure will be heavily taken profit of in the proof
+          whose structure will be taken profit of in the proof
           of the compiler.
        *)
       Definition denote_b (bs: bks A B): ktree E A B :=
@@ -211,16 +213,17 @@ Section Denote.
 
     End with_labels.
 
-  (** One can think of an asm program as a circuit/diagram where wires
+  (** One can think of an [asm] program as a circuit/diagram where wires
       correspond to jumps/program links.
       [denote_b] computes the meaning of each basic block as an [itree] that
       returns the label of the next block to jump to, laying down all our
       elementary wires. 
       In order to denote an [asm A B] program as a [ktree E A B], it therefore
       remains to wire all those denoted blocks together while hiding the
-      internal lables.
-      We accomplish this with the same [loop] combinator we used to denote
-      _Imp_'s [while] loop.
+      internal labels.
+      Luckily, that is exactly what traced monoidal category are good for.
+      We therefore accomplish this with the same [loop] combinator we used to
+      denote _Imp_'s [while] loop.
       It directly takes our [denote_b (code s): ktree E (I + A) (I + B)] and
       hides [I] as desired.
    *)
@@ -235,26 +238,21 @@ End Denote.
 (* ================================================================= *)
 (** ** Interpretation *)
 
+(* begin hide *)
 From ITree Require Import
+     Basics.Category
      Effects.Env.
 
 From ExtLib Require Import
      Core.RelDec
      Structures.Maps
      Data.Map.FMapAList.
+(* end hide *)
 
 (** Both environments and memory effects can be interpreted as "map" effects,
     exactly as we did for _Imp_. *)
 
-Definition interpret_Locals {E : Type -> Type} `{envE var value -< E} :
-  Locals ~> itree E :=
-  fun _ e =>
-    match e with
-    | GetVar x => env_lookupDefault x 0
-    | SetVar x v => env_add x v
-    end.
-
-Definition interpret_Memory {E : Type -> Type} `{envE value value -< E} :
+Definition evalMemory {E : Type -> Type} `{envE value value -< E} :
   Memory ~> itree E :=
   fun _ e =>
     match e with
@@ -263,22 +261,24 @@ Definition interpret_Memory {E : Type -> Type} `{envE value value -< E} :
     end.
 
 (** Once again, we implement our Maps with a simple association list *)
-Definition env := alist var value.
 Definition memory := alist value value.
 
-(* Enable typeclass instances for Maps keyed by strings and values *)
-Instance RelDec_string : RelDec (@eq string) :=
-  { rel_dec := fun s1 s2 => if String.string_dec s1 s2 then true else false}.
+(** We can then define an evaluator for closed assembly programs by
+    interpreting both store and heap events into two instances of [envE],
+    and running them both.
+ *)
+Definition AsmEval (p: asm unit void) :=
+  let p1 := interp (bimap evalLocals (id_ (Memory +' Exit))) _ (denote_asm p tt) in 
+  let p2 := interp (bimap (id_ _) (bimap evalMemory (id_ _))) _ p1 in
+  run_env _ (run_env _ p2 empty) empty.
 
-Instance RelDec_value : RelDec (@eq value) := { rel_dec := Nat.eqb }.
-
-(* TODO: redefine the interpreter *)
-(* Definition AsmEval (p: asm unit void): itree void1 (env * unit) := *)
-(*   let p := interp evalLocals _ (denoteStmt s) in *)
-(*   run_env _ p empty. *)
-(* Definition run (p: asm unit done) : itree emptyE (env * (memory * unit)) := *)
-(*   let eval := Sum1.elim interpret_Locals interpret_Memory in *)
-(*   run_env _ (run_env _ (interp eval _ (denote_asm p tt)) empty) empty. *)
-
-
-(**  *)
+(** Now that we have both our language, we could jump directly into implementing
+    our compiler.
+    However, if we look slightly ahead of us, we can observe that:
+    - compiling expressions and basic statements will be mostly straightforward;
+    - but linking the resulting elementary (open) [asm] programs together is not
+    as trivial. In particular, reasoning inductively on this linking is more
+    challenging.
+    We therefore take a detour: we first reason in isolation about linking, and
+    jump to this end to [AsmCombinators.v].
+ *)
