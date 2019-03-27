4292ac652087ddc980dfbab5dea0e7593d15d353
Lysxia <lysxia@gmail.com>
2019-03-27 16:59:37 -0400

    test: Update extraction test

--- tests/extraction/Extract.v
+++ tests/extraction/Extract.v
@@ -1,4 +1,10 @@
 Require TestExtraction.MetaModule.
 
 Require Extraction.
+
+Extraction Language OCaml.
+Extraction Blacklist String List Char Core Z.
+
+Cd "extraction".
 Recursive Extraction Library MetaModule.
+Cd "..".
