cad74446879ed72f2b20a0a5240a6573614240fe
Yannick <y.zakowski@gmail.com>
2019-06-17 07:28:44 -0400

    Experimenting with views to replace the previous notion of subevents

--- theories/Core/Subevent.v
+++ theories/Core/Subevent.v
@@ -682,6 +682,302 @@ Section Test.
 End Test.
 
 
+(*************** Instances ***************)
+
+(** Event level instances *)
+
+(* The subeffect relationship is reflexive
+   A <-> A -> void1 *)
+Instance View_id {A} : View A void1 A.
+refine
+  {| preview := inl1
+     ; review := fun _ x => x
+  |}.
+auto.
+intros ? ? ? H; inversion H; auto.
+Defined.
+
+(* void1 is a subeffect of any type
+   void1 <-> A -> A *)
+Instance View_none {A}: View A A void1.
+refine
+  {| preview := inr1
+     ; review := fun t (x: void1 t) => match x with end
+  |}.
+intros ? x; inversion x.
+intros ? ? x; inversion x.
+Defined.
+
+(* Extends the supertype to the right
+     a <-> A -> Z
+-------------------------
+ a <-> A +' B -> Z +' B
+ *)
+Instance View_R {A B Z a} {_ : View A Z a} : View (A +' B) (Z +' B) a.
+refine
+  {|
+    preview := fun _ X =>
+                 match X with
+                 | inl1 a => match preview _ a with
+                            | inl1 a2 => inl1 a2
+                            | inr1 z => inr1 (inl1 z)
+                            end
+                 | inr1 b => inr1 (inr1 b)
+                 end;
+    review := fun (T : Type) (x : a T) => inl1 (review X x)
+  |}.
+Proof.
+  - intros t x; cbn.
+    rewrite preview_review; reflexivity.
+  - intros t [x | y] x'; [| intros abs; inversion abs]; cbn. 
+    destruct (preview X x) eqn:EQ; [| intros abs; inversion abs].
+    intros eq; inversion eq; subst.
+    apply review_preview in EQ; rewrite EQ; reflexivity.
+Defined.
+
+(* Extends the supertype to the left
+     b <-> B -> Z
+-------------------------
+ b <-> A +' B -> Z +' A
+ *)
+
+Instance View_L {A B Z b} {_ : View B Z b} : View (A +' B) (Z +' A) b.
+refine
+  {|
+    preview :=  fun _ X =>
+                 match X with
+                 | inl1 a =>  inr1 (inr1 a)
+                 | inr1 b => match preview _ b with
+                            | inl1 b => inl1 b
+                            | inr1 z => inr1 (inl1 z)
+                            end
+                 end;
+    review := fun _ x => inr1 (review X x)
+  |}.
+Proof.
+  - intros t x; cbn.
+    rewrite preview_review; reflexivity.
+  - intros t [x | y] x'; [intros abs; inversion abs |]; cbn. 
+    destruct (preview X y) eqn:EQ; [| intros abs; inversion abs].
+    intros eq; inversion eq; subst.
+    apply review_preview in EQ; rewrite EQ; reflexivity.
+Defined.
+
+(* Builds combined domains of subeffects.
+   What is the right choice here for Z? Taking the sum of both domains is definitely sensible if
+   the instance resolution is such that we always first build 'pure effect views', and then on top
+   of it the view into the monad of interest.
+   Otherwise, we might end up with views into 'itree E +' itree F', which might be a bit too funky.
+     a <-> A -> Y
+     b <-> B -> Z
+-------------------------
+ a +' b <-> A' +' B' -> Y +' Z
+ *)
+Instance View_Sum {a A b B Y Z} {VA : View A Y a} {VB : View B Z b} : View (A +' B) (Y +' Z) (a +' b).
+refine
+  {|
+    preview := fun _ ab => 
+                 match ab with
+                 | inl1 a => match preview VA a with
+                            | inl1 a => inl1 (inl1 a)
+                            | inr1 y => inr1 (inl1 y)
+                            end
+                 | inr1 b => match preview VB b with
+                            | inl1 b => inl1 (inr1 b)
+                            | inr1 z => inr1 (inr1 z)
+                            end
+                 end;
+    review := fun _ ab =>
+                match ab with
+                | inl1 a => inl1 (review VA a)
+                | inr1 b => inr1 (review VB b)
+                end
+  |}.
+Proof.
+  - intros t [xa | xb]; cbn;
+      rewrite preview_review; reflexivity.
+  - intros t [xA | xB] [xa | xb];
+      match goal with
+        |- context [match ?h with | _ => _ end] => destruct h eqn:EQh
+      end; intros EQ; inv EQ; f_equal; apply review_preview; auto.
+Defined.
+
+(* We can always forget the domain into which we knew how to embed the complement
+ A <-> B -> Z
+------------------
+ A <-> B -> unit1
+ *)
+Instance SubFromView {A B Z} `{V:View A Z B}: B -< A :=
+  {|
+    preview := fun _ a => match V.(preview) a with 
+                       | inl1 b => inl1 b
+                       | inr1 _ => inr1 tt
+                       end;
+    review := V.(review)
+  |}.
+Proof.
+  intros; rewrite preview_review; reflexivity.
+  intros ? ? ? H; match type of H with context[match ?x with | _ => _ end] => destruct x eqn: EQ end; inv H.
+  apply review_preview; auto.
+Defined.
+
+(** Instances to call [over] into other monads *)
+
+(*
+ A <-> B -> Z
+------------------
+ A <-> B -> itree Z
+ *)
+Instance ViewToITree {A B Z} `{V: View A Z B}: View A (itree Z) B :=
+  {|
+    preview := fun _ x =>
+                 match V.(preview) x with
+                 | inl1 e => inl1 e
+                 | inr1 f => inr1 (ITree.trigger f)
+                 end;
+    review := V.(review)
+  |}.
+Proof.
+  - intros; rewrite preview_review; reflexivity.
+  - intros ? xy x; destruct (preview V xy) eqn:EQ; intros EQ'; inv EQ'; apply review_preview; auto.
+Defined.
+
+(*
+ A <-> B -> Z
+------------------
+ A <-> B -> stateT s Z
+ *)
+
+(* Instance ViewToStateT {S A Z B} `{V: View A Z B}: View A (Monads.stateT S Z) B. *)
+(* This instance seems impossible to write.
+   It requires to build a [Z (S * T)] from a [Z T] which we cannot do in general.
+   We can certainly build the specific instance for [Z ~ itree Y] for some Y.
+ *)
+Definition pure_state {S E} : E ~> Monads.stateT S (itree E)
+  := fun _ e s => Vis e (fun x => Ret (s, x)).
+
+Instance ViewToStateT {S A Z B} `{V: View A Z B}: View A (Monads.stateT S (itree Z)) B :=
+  {|
+    preview := fun _ a =>
+                 match preview V a with
+                 | inl1 b => inl1 b
+                 | inr1 z => inr1 (pure_state _ z)
+                 end;
+    review := review V
+  |}.
+Proof.
+  intros; rewrite preview_review; reflexivity.
+  - intros ? xy x; destruct (preview V xy) eqn:EQ; intros EQ'; inv EQ'; apply review_preview; auto.
+Defined.  
+
+(* We could define as many instances as we want to use nested monads in practice...
+   Can we do better? Gets back to the idea of having a "triggerable" type class.
+ *)
+
+Section Test.
+
+  (* Small stress test: can we infer a view instance picking event domains 1 and 3 in a list? *)
+  Variable A B C D: Type -> Type.
+  Goal View (A +' B +' C +' D) (B +' D) (A +' C).
+    (* Not with the instances above *)
+  Abort.
+
+  (* void1 is annoying. This permits to extend with two recursive instances when the instance to the left is reflexive  *)
+  Instance View_Sum' {a A b B Z} {VA : View A void1 a} {VB : View B Z b} : View (A +' B) Z (a +' b).
+  refine
+    {|
+      preview := fun _ ab => 
+                   match ab with
+                   | inl1 a => match preview VA a with
+                              | inl1 a => inl1 (inl1 a)
+                              | inr1 y => match y with end
+                              end
+                   | inr1 b => match preview VB b with
+                              | inl1 b => inl1 (inr1 b)
+                              | inr1 z => inr1 z
+                              end
+                   end;
+      review := fun _ ab =>
+                  match ab with
+                  | inl1 a => inl1 (review VA a)
+                  | inr1 b => inr1 (review VB b)
+                  end
+    |}.
+  Proof.
+    - intros t [xa | xb]; cbn;
+        rewrite preview_review; reflexivity.
+    - intros t [xA | xB] [xa | xb];
+        match goal with
+          |- context [match ?h with | _ => _ end] => destruct h eqn:EQh
+        end; intros EQ; inv EQ; f_equal; ((inv v; fail) || (apply review_preview; auto)).
+  Defined.
+
+  (* Seems to work better with the second argument ordered this way, to double check *)
+  Instance View_L' {A B Z b} {_ : View B Z b} : View (A +' B) (A +' Z) b.
+  refine
+    {|
+      preview := fun _ X =>
+                   match X with
+                   | inl1 a => inr1 (inl1 a)
+                   | inr1 b => match preview _ b with
+                              | inl1 b => inl1 b
+                              | inr1 z => inr1 (inr1 z)
+                              end
+                   end;
+      review := fun _ x => inr1 (review X x)
+    |}.
+  Proof.
+    - intros t x; cbn.
+      rewrite preview_review; reflexivity.
+    - intros t [x | y] x'; [intros abs; inversion abs |]; cbn. 
+      destruct (preview X y) eqn:EQ; [| intros abs; inversion abs].
+      intros eq; inversion eq; subst.
+      apply review_preview in EQ; rewrite EQ; reflexivity.
+  Defined.
+
+  (* Seems to work better with the second argument ordered this way, to double check *)
+  Instance View_R' {A B Z a} {_ : View A Z a} : View (A +' B) (B +' Z) a.
+  refine
+    {|
+      preview := fun _ X =>
+                   match X with
+                   | inl1 a => match preview _ a with
+                              | inl1 a2 => inl1 a2
+                              | inr1 z => inr1 (inr1 z)
+                              end
+                   | inr1 b => inr1 (inl1 b)
+                   end;
+      review := fun (T : Type) (x : a T) => inl1 (review X x)
+    |}.
+  Proof.
+    - intros t x; cbn.
+      rewrite preview_review; reflexivity.
+    - intros t [x | y] x'; [| intros abs; inversion abs]; cbn. 
+      destruct (preview X x) eqn:EQ; [| intros abs; inversion abs].
+      intros eq; inversion eq; subst.
+      apply review_preview in EQ; rewrite EQ; reflexivity.
+  Defined.
+
+  (* Alternate base case needed *)
+  Instance View_base {A B}: View (A +' B) B A.
+  refine
+    {|
+      preview := fun _ ab => ab;
+      review := inl1
+    |}.
+  Proof.
+    - auto.
+    - auto. 
+  Defined.
+
+  Goal View (A +' B +' C +' D) (B +' D) (A +' C).
+    (* Now yes. Needs some thinking, a bit of a mess right now *)
+    typeclasses eauto.
+  Qed.
+
+End Test.
+
+
 (* Embedding events into trees.
 
    For example:
