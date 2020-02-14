331c6e1c02dd698d010ef255b66d9af3d146b631
euisuny <ey222@cornell.edu>
2020-02-14 14:15:07 -0500

    Stated op semantics and strong and weak bisimulation of GuardedCCS

--- examples/ccs/GuardedCCS.v
+++ examples/ccs/GuardedCCS.v
@@ -73,12 +73,12 @@ Section GuardedCCS.
 
   (* CCS labels have polarity. *)
   Variant visible : Type :=
-  | In (x : idx)
-  | Out (x : idx)
+  | In (i : idx)
+  | Out (i : idx)
   .
 
   Variant label : Type :=
-  | Visible (x : visible)
+  | Visible (vx : visible)
   | Silent
   .
 
@@ -125,23 +125,185 @@ Section GuardedCCS.
   Inductive seq_ccs : Type :=
   | Zero
   | Or (a b : seq_ccs)
-  | Act (x : label) (a : seq_ccs)
+  | Act (l : label) (a : seq_ccs)
   .
 
   (* CCS Processes. *)
   Inductive ccs : Type :=
   | Proc (p : seq_ccs)
   | Par (a b : ccs)
-  | New (x : idx) (a : ccs)
+  | New (i : idx) (a : ccs)
   | Bang (p : ccs)
+  .
+
+  Infix "≡?" := (eq_idx) (at level 70).
+
+  (** *Structural Congruence *)
+  Inductive scong : seq_ccs -> seq_ccs -> Prop :=
+  | SC_Sum (a b : seq_ccs):
+      scong (Or a b) (Or b a)
+  | SC_CongOr (a a' b b' : seq_ccs):
+      scong a a' -> scong b b' ->
+      scong (Or a b) (Or a' b')
+  | SC_CongAct (l : label) (a a' : seq_ccs):
+      scong a a' ->
+      scong (Act l a) (Act l a')
+  .
+
+  Inductive s_bound : idx -> seq_ccs -> Prop :=
+  | SB_ActIn (i : idx) (a : seq_ccs):
+      s_bound i (Act (Visible (In i)) a)
+  | SB_ActOut (i : idx) (a : seq_ccs):
+      s_bound i (Act (Visible (Out i)) a)
+  | SB_Act (i : idx) (x : label) (a : seq_ccs):
+      s_bound i a ->
+      s_bound i (Act x a)
+  | SB_Or (i : idx) (a b : seq_ccs):
+      s_bound i a \/ s_bound i b ->
+      s_bound i (Or a b)
+  .
+
+  Inductive bound : idx -> ccs -> Prop :=
+  | B_Proc (i : idx) (a : seq_ccs):
+      s_bound i a ->
+      bound i (Proc a)
+  | B_Par (i : idx) (a b : ccs):
+      bound i a \/ bound i b ->
+      bound i (Par a b)
+  | B_New (i j : idx) (a : ccs):
+      bound i a -> not (i ≡? j) ->
+      bound i (New j a)
+  | B_Bang (i : idx) (a : ccs):
+      bound i a ->
+      bound i (Bang a)
+  .
+
+  Definition free (i : idx) (c : ccs) : Prop := not (bound i c).
+
+  Inductive cong : ccs -> ccs -> Prop :=
+  | C_Proc (a b : seq_ccs) :
+      scong a b ->
+      cong (Proc a) (Proc b)
+  | C_ParZero (a : ccs):
+      cong (Par (Proc Zero) a) (Par a (Proc Zero))
+  | C_ParComm (a b : ccs):
+      cong (Par a b) (Par b a)
+  | C_ParAssoc (a b c : ccs):
+      cong (Par (Par a b) c) (Par a (Par b c))
+  | C_NewDist (i : idx) (a b : ccs):
+      free i a ->
+      cong (New i (Par a b)) (Par a (New i b))
+  | C_NewZero (i : idx) :
+      cong (New i (Proc Zero)) (Proc Zero)
+  | C_Bang (a : ccs) :
+      cong (Bang a) (Par a (Bang a))
+  | C_CongPar (a a' b b': ccs):
+      cong a a' -> cong b b' ->
+      cong (Par a b) (Par a' b')
+  | C_CongNew (i : idx) (a a' : ccs):
+      cong a a' ->
+      cong (New i a) (New i a')
+  | C_CongBang (a a' : ccs):
+      cong a a' ->
+      cong (Bang a) (Bang a')
   .
 
   (** *Structural Operational Semantics *)
 
+  Inductive seval : option label -> seq_ccs -> seq_ccs -> Prop :=
+  | SE_Pref (x : label) (a : seq_ccs):
+      seval (Some x) (Act x a) a
+  | SE_SumL (x : label) (a a' b : seq_ccs):
+      seval (Some x) a a' ->
+      seval (Some x) (Or a b) a'
+  | SE_SumR (x : label) (a b b' : seq_ccs):
+      seval (Some x) b b' ->
+      seval (Some x) (Or a b) b'
+  .
+
+  (* 'None' option is used for structural congruence. *)
+  Inductive eval : option label -> ccs -> ccs -> Prop :=
+  | E_Proc (x : option label) (a a': seq_ccs):
+      seval x a a' ->
+      eval x (Proc a) (Proc a')
+  | E_ParL (x : option label) (a a' b : ccs):
+      eval x a a' ->
+      eval x (Par a b) (Par a' b)
+  | E_ParR (x : option label) (a b b' : ccs):
+      eval x b b' ->
+      eval x (Par a b) (Par a b')
+  | E_ParC (i : idx) (a a' b b' : ccs):
+      eval (Some (Visible (In i))) a a' ->
+      eval (Some (Visible (Out i))) b b' ->
+      eval (Some Silent) a' b'
+  | E_ResIn (i j : idx) (a a' : ccs):
+      not (i ≡? j) ->
+      eval (Some (Visible (In i))) a a' ->
+      eval (Some (Visible (In i))) (New j a) (New j a')
+  | E_ResOut (i j : idx) (a a' : ccs):
+      not (i ≡? j) ->
+      eval (Some (Visible (Out i))) a a' ->
+      eval (Some (Visible (Out i))) (New j a) (New j a')
+  | E_ResSilent (j : idx) (a a': ccs):
+      eval (Some Silent) a a' ->
+      eval (Some Silent) (New j a) (New j a')
+  | E_Struct (x : option label) (a a' b b': ccs):
+      cong a b \/ a = b -> cong a' b' \/ a' = b' ->
+      eval x a a' -> eval x b b'
+  .
+
+  (** *Strong Bisimulation *)
+  Inductive strong_simulation : ccs -> ccs -> Prop :=
+  | StrongSim (p q : ccs) :
+      (forall a p', eval a p p' ->
+      (exists q', eval a q q' /\ strong_simulation p' q')) ->
+     strong_simulation p q
+  .
+
+  Inductive strong_bisimulation : ccs -> ccs -> Prop :=
+  | StrongBisim (p q : ccs):
+      strong_simulation p q -> strong_simulation q p ->
+      strong_bisimulation p q.
+
+  (** *Weak Bisimulation *)
+
+  Inductive weak_silent_closure : ccs -> ccs -> Prop :=
+  | WeakSilentRefl (p : ccs):
+      weak_silent_closure p p
+  | WeakSilentTrans (p q r : ccs):
+      weak_silent_closure q r ->
+      eval (Some Silent) p q ->
+      weak_silent_closure p r
+  .
+
+  Inductive weak_visible_closure : ccs -> ccs -> Prop :=
+  | WeakVisRefl x (p q r: ccs):
+      weak_silent_closure q r ->
+      eval (Some (Visible x)) p q ->
+      weak_visible_closure p r
+  | WeakVisTrans x (p q r: ccs):
+      weak_visible_closure q r ->
+      eval (Some (Visible x)) p q ->
+      weak_visible_closure p r
+  .
+
+  Inductive weak_simulation : ccs -> ccs -> Prop :=
+  | WeakSim (p q : ccs) :
+      (forall p', eval (Some Silent) p p' ->
+             exists q', weak_silent_closure q q' /\ weak_simulation p' q') ->
+      (forall x p', eval (Some (Visible x)) p p' ->
+               exists q', weak_visible_closure q q' /\ weak_simulation p' q') ->
+      weak_simulation p q
+  .
+
+  Inductive weak_bisimulation : ccs -> ccs -> Prop :=
+  | WeakBisim (p q : ccs):
+      weak_simulation p q -> weak_simulation q p ->
+      weak_bisimulation p q
+  .
 
 End GuardedCCS.
 
-Infix "≡?" := (eq_idx) (at level 70).
 
 (** *Denotational Semantics
 
@@ -213,7 +375,7 @@ Section DenoteCCS.
              | inl a => Vis ParE (fun (b : bool) => if b then Tau (a) else bang body a)
              | inr b => Ret b (* never going to be reached *)
              end
-   .
+  .
 
   Fixpoint denote_ccs (p : ccs) : itree eff unit :=
     match p with
