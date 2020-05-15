1842595b2e7e2d3edb5dff33e1953b64ee0ed765
euisuny <ey222@cornell.edu>
2020-05-14 21:46:11 -0400

    Some clean up

--- theories/Basics/PropT.v
+++ theories/Basics/PropT.v
@@ -13,108 +13,6 @@ From ITree Require Import
      Basics.CategoryMonad
 .
 
-(*  We are working in a category C.
-    Objects:    Typ
-    Hom (Typ eqA) (Typ eqB) := { f | Proper (eqA ==> eqB) f }
-    ID in Hom (Typ eqA) (Typ eqA) := fun (x:A) => x
-*)
-Section TypCat.
-
-  Definition typ_proper (TA TB : typ) := {f | Proper (equalE TA ==> equalE TB) f}.
-
-  Instance eq2_typ_proper : Eq2 typ_proper :=
-    (fun a b tp tp' =>
-       let f := proj1_sig tp in
-       let g := proj1_sig tp' in
-       forall x y, x ∈ a -> y ∈ a -> equalE a x y -> equalE b (f x) (g y)).
-
-  Lemma id_ok: forall (TA : typ),
-      Proper (equalE TA ==> equalE TA) (fun x => x).
-  Proof.
-    intros.
-    repeat red. tauto.
-  Qed.
-
-  Lemma compose: forall (TA TB TC : typ) (f : TA -> TB) (g : TB -> TC)
-      (P1: Proper (equalE TA ==> equalE TB) f)
-      (P2: Proper (equalE TB ==> equalE TC) g),
-      Proper (equalE TA ==> equalE TC) (fun x => g (f x)).
-  Proof.
-    intros TA TB TC f g P1 P2.
-    repeat red.
-    intros.
-    apply P2. apply P1. assumption.
-  Qed.
-
-  Instance id_typ_proper : Id_ typ_proper :=
-    fun TA : typ =>
-    exist (fun f : TA -> TA => Proper (equalE TA ==> equalE TA) f)
-      (fun x : TA => x) (id_ok TA).
-
-  Instance cat_typ_proper : Cat typ_proper :=
-    fun (a b c : typ) (TA : typ_proper a b) (TB : typ_proper b c) =>
-    exist (fun f : a -> c => Proper (equalE a ==> equalE c) f)
-      (fun x : a => (` TB) ((` TA) x))
-      (compose a b c (` TA) (` TB) (proj2_sig TA) (proj2_sig TB)).
-
-  Instance cat_IdL_typ_proper : CatIdL typ_proper.
-    repeat intro. destruct f. cbn. apply p. assumption.
-  Defined.
-
-  Instance cat_IdR_typ_proper : CatIdR typ_proper.
-    repeat intro. destruct f. cbn. apply p. assumption.
-  Defined.
-
-  Instance cat_assoc_typ_proper : CatAssoc typ_proper.
-    refine (fun a b c d TA TB TC => _). repeat intro.
-    destruct TA, TB, TC. eapply p1. eapply p0. eapply p. assumption.
-  Defined.
-
-  Instance proper_typ_proper (a b c : typ) : Proper ((@eq2 typ _ _ a b) ==> (@eq2 typ _ _ b c) ==> (@eq2 typ _ _ a c)) (cat).
-    repeat intro.
-    destruct x, y, x0, y0. unfold eq2, eq2_typ_proper in H0.
-    cbn in H0. unfold eq2, eq2_typ_proper in H. cbn in H. cbn.
-    specialize (H x1 y1 H1 H2 H3).
-    specialize (H0 (x x1) (x2 y1)).
-    apply H0. 3 : apply H. apply p. apply H1. apply p0. apply H2.
-  Defined.
-
-  Global Instance category_typ_proper : Category typ_proper.
-    constructor; try typeclasses eauto.
-  Defined.
-
-End TypCat.
-
-(* Section MonadLaws. *)
-
-
-(*   Class MonadProperties : Prop := *)
-(*     { *)
-(*       (* mon_ret_proper  :> forall {A : typ} `{PER A (equalE A)}, *) *)
-(*       (*     Proper ((equalE A) ==> feq) ret; *) *)
-
-(*       (* mon_bind_proper :> forall {A B : typ} `{PER A (equalE A)} `{PER B (equalE B)}, *) *)
-(*       (*                 Proper (feq ==> (equalE A ==> feq) ==> feq) *) *)
-(*       (*                 bind; *) *)
-
-(*       bind_ret_l : forall {A B : typ} `{PER A (equalE A)} `{PER B (equalE B)} *)
-(*           (f : A -> M B) (PF:Proper (equalE A ==> feq) f), *)
-(*         (equalE (equalE A ~~> feq)) (fun (a:A) => bind (ret a) f)  f; *)
-
-(*       bind_ret_r : forall {A : typ} `{PER A (equalE A)}, *)
-(*           (equalE (feq ~~> feq)) (fun x => bind x ret) (id); *)
-
-(*       bind_bind : forall {A B C : typ} *)
-(*                     `{PER A (equalE A)} `{PER B (equalE B)} `{PER C (equalE C)} *)
-(*                     (f : A -> M B) (g : B -> M C) *)
-(*                     (PF:Proper (equalE A ==> feq) f)  (* f \in TYP (eqa ~~> eqb) *) *)
-(*                     (PG:Proper (equalE B ==> feq) g), *)
-(*         (equalE (feq ~~> feq)) *)
-(*           (fun (x: M A) => (@bind M _ B C (bind x f) g)) *)
-(*           (fun (x: M A) => (@bind M _ A C x (fun (y : A) => (bind (f y) g)))) *)
-(*     }. *)
-(* End MonadLaws. *)
-
 Import CatNotations.
 Open Scope cat_scope.
 
@@ -135,86 +33,22 @@ Section MonadProp.
     split. intros. apply H0, H, H1. intros. apply H, H0, H1.
   Qed.
 
+  (* TODO Prove MonadLaws for Prop. *)
   Instance PropM_Monad : Monad typ_proper PropM.
   split.
   - repeat red.
     intros.
-    refine (exist _ (fun (x:a) =>  (exist _ (fun y => equalE a x y) _)) _).    
+    refine (exist _ (fun (x:a) =>  (exist _ (fun y => equalE a x y) _)) _).
   Admitted.
 
 End MonadProp.
-Section NaturalTransformation.
 
-  Context {obj1 obj2 : Type}
-          {C1 : obj1 -> obj1 -> Type}
-          {C2 : obj2 -> obj2 -> Type}
-          `{Cat _ C1} `{Cat _ C2} `{Eq2 _ C1} `{Eq2 _ C2}
-          {F : obj1 -> obj2}
-          {G : obj1 -> obj2}
-          {F_FUN : Functor (C1 := C1) (C2 := C2) F}
-          {G_FUN : Functor (C1 := C1) (C2 := C2) G}.
-
-  Class NaturalTransformation : Type :=
-    {
-      eta: forall (a : obj1), C2 (F a) (G a)
-    }.
-
-  Context `{NaturalTransformation}.
-  Class NaturalTransformationLaw : Prop :=
-    {
-      naturality: forall (a b : obj1) (f : C1 a b),
-        fmap (F := F) _ _ f >>> eta b ⩯ eta a >>> fmap (F := G) _ _ f
-    }.
-
-End NaturalTransformation.
-
-
-Section MonadTransformer.
-  (* Instance Natural_MonadTransfomer : MonadTransformer N.  *)
-
-  (* OPTION 2 : Have a NaturalTransformation Instance in Context. *)
-  Context {obj: Type}
-          {C: obj-> obj-> Type}
-          `{Cat _ C} `{Eq2 _ C}
-          {M : obj-> obj}
-          {N : obj-> obj}
-  .
-
-  (* Class MonadTransfomer : Type := *)
-  (*   { *)
-  (*     m : Monad C M; *)
-  (*     n : Monad C N; *)
-  (*     natural_transformation :> @NaturalTransformation _ _ C M N; *)
-  (*     natural_transformation_law :> @NaturalTransformationLaw _ _ C C _ _ M N *)
-  (*   }. *)
-
-  (* TODO Prove MonadTransformer laws using natural transformation laws. *)
-  (* TODO Give StateT Instance of MonadTransformer. *)
-  (* TODO Prove MonadLaws for Prop. *)
-
-End MonadTransformer.
-
-(* TODO Prove MonadLaws for Prop. *)
 Section MonadPropT.
 
   Context {M : typ -> typ}.
   Context `{F : Functor typ typ typ_proper typ_proper M}.
-  Context `{FL : FunctorLaws typ typ typ_proper typ_proper M}.
   Context `{CM : Monad typ typ_proper M}.
   Context `{ML : MonadLaws typ typ_proper M}.
-
-  (*
-     PropT : TYP -> TYP
-
-     Instance Prop_TYP : TYP :=
-       {
-         A = Prop;
-         eqa := fun p q => forall x, eqx x p x <-> q x
-       }
-   *)
-
-  (* IY: feq is equalE (M X). *)
-  (* Context {feq : forall (A : typ), rel (M A)}. *)
 
   Definition PropT : typ -> typ :=
     fun (X : typ) =>
@@ -379,3 +213,34 @@ Section MonadPropLaws.
 
                                                   
 *)
+
+
+(* Section MonadLaws. *)
+
+
+(*   Class MonadProperties : Prop := *)
+(*     { *)
+(*       (* mon_ret_proper  :> forall {A : typ} `{PER A (equalE A)}, *) *)
+(*       (*     Proper ((equalE A) ==> feq) ret; *) *)
+
+(*       (* mon_bind_proper :> forall {A B : typ} `{PER A (equalE A)} `{PER B (equalE B)}, *) *)
+(*       (*                 Proper (feq ==> (equalE A ==> feq) ==> feq) *) *)
+(*       (*                 bind; *) *)
+
+(*       bind_ret_l : forall {A B : typ} `{PER A (equalE A)} `{PER B (equalE B)} *)
+(*           (f : A -> M B) (PF:Proper (equalE A ==> feq) f), *)
+(*         (equalE (equalE A ~~> feq)) (fun (a:A) => bind (ret a) f)  f; *)
+
+(*       bind_ret_r : forall {A : typ} `{PER A (equalE A)}, *)
+(*           (equalE (feq ~~> feq)) (fun x => bind x ret) (id); *)
+
+(*       bind_bind : forall {A B C : typ} *)
+(*                     `{PER A (equalE A)} `{PER B (equalE B)} `{PER C (equalE C)} *)
+(*                     (f : A -> M B) (g : B -> M C) *)
+(*                     (PF:Proper (equalE A ==> feq) f)  (* f \in TYP (eqa ~~> eqb) *) *)
+(*                     (PG:Proper (equalE B ==> feq) g), *)
+(*         (equalE (feq ~~> feq)) *)
+(*           (fun (x: M A) => (@bind M _ B C (bind x f) g)) *)
+(*           (fun (x: M A) => (@bind M _ A C x (fun (y : A) => (bind (f y) g)))) *)
+(*     }. *)
+(* End MonadLaws. *)
--- theories/Basics/Typ_Class2.v
+++ theories/Basics/Typ_Class2.v
@@ -28,9 +28,9 @@ Definition rel A := A -> A -> Prop.
  *)
 Class typ : Type :=
   Typ {
-      Ty : Type; 
+      Ty : Type;
       equal : rel Ty;
-      equal_PER :> PER equal                  
+      equal_PER :> PER equal
     }.
 Arguments equal {T}: rename.
 Arguments Typ {Ty} _ {equal_PER}.
@@ -55,7 +55,7 @@ Notation "a ∈ T" := (contains T a) (at level 75).
 Fact type_eq_full : forall A (a: A), a ∈ (typ_eq A).
 Proof.
   reflexivity.
-Qed.  
+Qed.
 
 (** ** top, bot
     Given a carrier [A], two particular [typ A] can be defined: [top] where no elements are distinguished,
@@ -71,7 +71,7 @@ Instance bot_PER {A} : PER (fun a b : A => False).
 Proof.
   split; eauto.
 Qed.
-  
+
 Definition top_typ A : typ := Typ (fun a b : A => True).
 Definition bot_typ A : typ := Typ (fun a b : A => False).
 
@@ -79,13 +79,13 @@ Definition bot_typ A : typ := Typ (fun a b : A => False).
 Fact top_typ_full : forall A (a: A), a ∈ (top_typ A).
 Proof.
   reflexivity.
-Qed.  
+Qed.
 
 (* But none are in [bot_typ] *)
 Fact bot_typ_empty : forall A (a: A), ~a ∈ (bot_typ A).
 Proof.
   intros ? ? abs; inversion abs.
-Qed.  
+Qed.
 
 (** ** prod
     Cartesian product of two [typ].
@@ -99,16 +99,6 @@ Proof.
   repeat red. intros x y (H1 & H2). split; symmetry; assumption.
   repeat red. intros x y z (H1 & H2) (H3 & H4). split; eapply transitivity; eauto.
 Qed.
-
-(*
-Instance pair_PER (TA TB:typ) :
-  PER (fun (p q: TA * TB) => equal (fst p) (fst q) /\ equal (snd p) (snd q)).
-Proof.
-  split.
-  repeat red. intros x y (H1 & H2). split; symmetry; assumption.
-  repeat red. intros x y z (H1 & H2) (H3 & H4). split; eapply transitivity; eauto.
-Qed.
-*)  
 
 Instance prod_typ (TA TB: typ) : typ :=
   Typ (fun (p q: TA * TB) => equal (fst p) (fst q) /\ equal (snd p) (snd q)).
@@ -126,9 +116,9 @@ Instance fun_PER (A B:Type) (RA : rel A) `{PER A RA} (RB : rel B) `{PER B RB}
 Proof.
   split.
   - repeat red. intros x y H1 a1 a2 H2.  symmetry. apply H1. symmetry. apply H2.
-  - repeat red. intros x y z H1 H2 a1 a2 H3. 
+  - repeat red. intros x y z H1 H2 a1 a2 H3.
     eapply transitivity. apply H1. apply H3. apply H2. eapply transitivity. symmetry. apply H3. apply H3.
-Qed.    
+Qed.
 
 (** ** arr
     Exponential of two [typ].
@@ -148,3 +138,78 @@ Goal ((Typ (@eq nat)) ~~> (bot_typ nat)).
 Proof.
   Fail typeclasses eauto. (* Still no instance with new Typ definition. *)
 Abort.
+
+
+(*
+    Typ forms a Category. We are working in a category C, where:
+
+    Objects:    Typ
+    Hom (Typ eqA) (Typ eqB) := { f | Proper (eqA ==> eqB) f }
+    ID in Hom (Typ eqA) (Typ eqA) := fun (x:A) => x
+*)
+Section TypCat.
+
+  Definition typ_proper (TA TB : typ) := {f | Proper (equalE TA ==> equalE TB) f}.
+
+  Instance eq2_typ_proper : Eq2 typ_proper :=
+    (fun a b tp tp' =>
+       let f := proj1_sig tp in
+       let g := proj1_sig tp' in
+       forall x y, x ∈ a -> y ∈ a -> equalE a x y -> equalE b (f x) (g y)).
+
+  Lemma id_ok: forall (TA : typ),
+      Proper (equalE TA ==> equalE TA) (fun x => x).
+  Proof.
+    intros.
+    repeat red. tauto.
+  Qed.
+
+  Lemma compose: forall (TA TB TC : typ) (f : TA -> TB) (g : TB -> TC)
+      (P1: Proper (equalE TA ==> equalE TB) f)
+      (P2: Proper (equalE TB ==> equalE TC) g),
+      Proper (equalE TA ==> equalE TC) (fun x => g (f x)).
+  Proof.
+    intros TA TB TC f g P1 P2.
+    repeat red.
+    intros.
+    apply P2. apply P1. assumption.
+  Qed.
+
+  Instance id_typ_proper : Id_ typ_proper :=
+    fun TA : typ =>
+    exist (fun f : TA -> TA => Proper (equalE TA ==> equalE TA) f)
+      (fun x : TA => x) (id_ok TA).
+
+  Instance cat_typ_proper : Cat typ_proper :=
+    fun (a b c : typ) (TA : typ_proper a b) (TB : typ_proper b c) =>
+    exist (fun f : a -> c => Proper (equalE a ==> equalE c) f)
+      (fun x : a => (` TB) ((` TA) x))
+      (compose a b c (` TA) (` TB) (proj2_sig TA) (proj2_sig TB)).
+
+  Instance cat_IdL_typ_proper : CatIdL typ_proper.
+    repeat intro. destruct f. cbn. apply p. assumption.
+  Defined.
+
+  Instance cat_IdR_typ_proper : CatIdR typ_proper.
+    repeat intro. destruct f. cbn. apply p. assumption.
+  Defined.
+
+  Instance cat_assoc_typ_proper : CatAssoc typ_proper.
+    refine (fun a b c d TA TB TC => _). repeat intro.
+    destruct TA, TB, TC. eapply p1. eapply p0. eapply p. assumption.
+  Defined.
+
+  Instance proper_typ_proper (a b c : typ) : Proper ((@eq2 typ _ _ a b) ==> (@eq2 typ _ _ b c) ==> (@eq2 typ _ _ a c)) (cat).
+    repeat intro.
+    destruct x, y, x0, y0. unfold eq2, eq2_typ_proper in H0.
+    cbn in H0. unfold eq2, eq2_typ_proper in H. cbn in H. cbn.
+    specialize (H x1 y1 H1 H2 H3).
+    specialize (H0 (x x1) (x2 y1)).
+    apply H0. 3 : apply H. apply p. apply H1. apply p0. apply H2.
+  Defined.
+
+  Global Instance category_typ_proper : Category typ_proper.
+    constructor; try typeclasses eauto.
+  Defined.
+
+End TypCat.
