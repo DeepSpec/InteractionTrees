f3b3307eb18f4127d193bcd8170e5d5056238624
Li-yao Xia <lysxia@gmail.com>
2021-03-07 14:29:36 -0500

    Remove useless script

--- setup-dune.sh
+++
@@ -1,5 +0,0 @@
-# Prepare repo for dune users
-cp _CoqProject.dune _CoqProject
-cp tutorial/_CoqProject.dune tutorial/_CoqProject
-cp examples/_CoqProject.dune examples/_CoqProject
-cp tests/_CoqProject.dune tests/_CoqProject
