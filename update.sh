04317e0dea121a315e4240e3ed90167cf9cb57e6
Li-yao Xia <lysxia@gmail.com>
2021-03-14 15:37:39 -0400

    script: Clean out any Dune configuration

--- update.sh
+++ update.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+# Clear dune _CoqProject before building
+if [ -f ../_CoqProject ]; then
+  diff -q ../_CoqProject ../_CoqProject.dune
+  if [ 0 -eq "$?" ] ; then rm ../_CoqProject; fi
+fi
+
 set -xe
-make -C .. html
+make -C .. html -j2
 DOCDIR=docs/master
 rm -rf $DOCDIR
 mv ../html $DOCDIR
 sh ./cleanup.sh
 set +x
 echo "OK!"
-echo "Now do this > git add -u ; git commit -m \"Update\""
+echo "Now do this > git add -u ; git status ; git commit -m \"Update\""
