2415cafa821bab7a6af60998a437ec5f79d6f5c3
Lucas Silver <lis32@cornell.edu>
2021-06-09 11:19:12 -0400

    slight update to Exception.v

--- theories/Events/Exception.v
+++ theories/Events/Exception.v
@@ -52,12 +52,66 @@ Definition throw {Err : Type} {E : Type -> Type} `{exceptE Err -< E} {X}
 
 (* translate *)
 
-(*
-Program Definition try_catch {Err R : Type } {E : Type -> Type} 
-           
-           (ttry : itree (exceptE Err +' E) R) (kcatch : Err -> itree (exceptE Err +' E) R) : itree (exceptE Err +' E) R :=
-  let ttry' : itree (exceptE Err +' (exceptE Err +' E)) R := 
-              translate (fun _ e => match e with |  inl1 e' => inl1 e' | inr1 e' => (inr1 (inr1 e') ) end ) ttry in
-  mrec (fun _ e => 
-          match e with Throw _ err => translate (fun _ e => inr1 e) (kcatch err) end) ttry'.
-*)
+Definition try_catch {Err R : Type } {E : Type -> Type} 
+            (ttry : itree (exceptE Err +' E) R) (kcatch : Err -> itree (exceptE Err +' E) R) : itree (exceptE Err +' E) R :=
+  (* the problem is kcatch is actually not a handler, need basic iter?*)
+  let catch_body (t : itree (exceptE Err +' E) R) : itree (exceptE Err +' E) ((itree (exceptE Err +' E) R) + R )  := 
+       match observe t with
+       | RetF r => Ret (inr r)
+       | TauF t' => Ret (inl t')
+       | VisF e k =>
+         match e with
+         | inl1 (Throw _ exc) => Functor.fmap inr (kcatch exc)
+         | inr1 e' =>  Functor.fmap (fun x => inl (k x) ) (trigger e) end end
+
+  in 
+  iter catch_body ttry.
+
+
+Definition throw_prefix {Err R : Type} {E : Type -> Type}
+           (t : itree (exceptE Err +' E) R) : itree  (exceptE Err +' E) (R + Err) :=
+  let prefix_body (t' : itree (exceptE Err +' E)  R ) : itree (exceptE Err +' E)  ((itree (exceptE Err +' E) R) + (R + Err) ) :=
+      match observe t' with
+      | RetF r => Ret (inr (inl r) )
+      | TauF t' => Ret (inl t')
+      | VisF e k =>
+        match e with
+        | inl1 (Throw _ exc) => Ret (inr (inr exc) )
+        | inr1 e' =>  Functor.fmap (fun x => inl (k x) ) (trigger e)
+        end
+      end in
+  iter prefix_body t.
+
+
+Lemma try_catch_ret : forall E Err R r (kcatch : Err -> itree (exceptE Err +' E) R),
+    try_catch (Ret r) kcatch ≅ Ret r.
+Proof.
+  intros. unfold try_catch. unfold iter, Iter_Kleisli, Basics.iter, MonadIter_itree.
+  rewrite unfold_iter. cbn. rewrite bind_ret_l. reflexivity.
+Qed.
+
+Lemma try_catch_tau : forall E Err R t (kcatch : Err -> itree (exceptE Err +' E) R),
+    try_catch (Tau t) kcatch ≅ Tau (try_catch t kcatch).
+Proof.
+  intros. unfold try_catch. unfold iter, Iter_Kleisli, Basics.iter, MonadIter_itree.
+  rewrite unfold_iter. cbn. rewrite bind_ret_l. reflexivity.
+Qed.
+
+Lemma try_catch_exc : forall E Err R exc (k :void -> itree (exceptE Err +' E) R) 
+                               (kcatch : Err -> itree (exceptE Err +' E) R),
+    try_catch (Vis (inl1 (Throw _ exc)) k ) kcatch ≅ kcatch exc.
+Proof.
+   intros. unfold try_catch. unfold iter, Iter_Kleisli, Basics.iter, MonadIter_itree.
+   rewrite unfold_iter. cbn. unfold ITree.map.
+   rewrite bind_bind. setoid_rewrite bind_ret_l. rewrite bind_ret_r.
+   reflexivity.
+Qed.
+
+Lemma try_catch_ev : forall E A Err R (ev: E A) k (kcatch : Err -> itree (exceptE Err +' E) R),
+    try_catch (Vis (inr1 ev) k ) kcatch ≅ Vis (inr1 ev) (fun x => Tau (try_catch (k x) kcatch) ).
+Proof.
+  intros. unfold try_catch. unfold iter, Iter_Kleisli, Basics.iter, MonadIter_itree.
+  rewrite unfold_iter. cbn. unfold ITree.map at 3.
+  rewrite bind_bind. rewrite bind_trigger. cbn.
+  setoid_rewrite bind_ret_l. reflexivity.
+Qed.
