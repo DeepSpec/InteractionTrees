9b3182dda8c0bf4e6954f2fcb7bb3328ea29c06e
Roger Burtonpatel <burtonpatel1@gmail.com>
2026-04-21 13:32:04 -0400

    removed a print stmt

--- theories/Eq/Eqit.v
+++ theories/Eq/Eqit.v
@@ -244,7 +244,6 @@ Experimenting with the latter at the moment. *)
 
 (* We first enhance the coinduction tactic to recognize goals that 
    do not have a syntactic match with [gfp _] *)
-Locate revert_last. 
 #[local] Ltac iunfold_all :=
   unfold euttge, eq_itree, eutt, eqit in *.
 
